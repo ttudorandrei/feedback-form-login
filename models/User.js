d54c681429c6d67a145c124f56d50318bc612745
Tudor Tocan <tudor.tocan@gmail.com>
2022-09-04 10:08:17 +0100

    add user model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: true,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "User",
+};
+
+User.init(schema, options);
+
+module.exports = User;
