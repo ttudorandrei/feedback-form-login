8b5265983cab600df80781134a639a27c19ebcc4
Tudor Tocan <tudor.tocan@gmail.com>
2022-09-04 10:17:55 +0100

    add session middleware

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const expressSession = require("express-session");
+// const connectSessionSequelize = require("connect-session-sequelize");
 
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: false,
+  rolling: true,
+  cookie: { maxAge: 60000 },
+};
+
+app.use(expressSession(sessionOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
