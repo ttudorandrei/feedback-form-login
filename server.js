ec0ff51bd1266174b713a4adacb1b7e38139d20e
Tudor Tocan <tudor.tocan@gmail.com>
2022-09-04 09:55:38 +0100

    refactor server to run based on connection to sequelize

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 
+const sequelize = require("./config/connection");
+
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+const init = async () => {
+  try {
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+init();
