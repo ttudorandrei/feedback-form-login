36fad9b88908e9aaf0aae8dc731984b994d53c8b
Tudor Tocan <tudor.tocan@gmail.com>
2022-09-10 10:56:38 +0100

    add endpoint to render signup page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const authRoutes = require("./auth");
+const homeRoutes = require("./homeRoutes");
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
